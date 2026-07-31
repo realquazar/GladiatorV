b44ba6849fb7111e6aa78c12509081b440f043b7
realquazar <pjohoottoa@gmail.com>
2026-07-31 20:19:29 +0530

    changing status

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ intents.message_content = True
 # Set the activity and status to online
 activity = nextcord.Activity(
     type=nextcord.ActivityType.playing,
-    name="🎉Our website is officially live! /help"
+    name="."
 )
 
 bot = commands.Bot(
